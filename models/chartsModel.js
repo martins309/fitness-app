0d9f283ee76f05b521bf14da63870087189485e3
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-02-13 15:16:42 -0500

    Charts working and responsive based upon type of workout. They are currently on their own page and need added to the profile and/or a progress page

--- models/chartsModel.js
+++ models/chartsModel.js
@@ -6,9 +6,28 @@ class Charts {
     constructor (id) {
         this.id = id;
     }
-    static async getTotalWeightByDate(date) {
+    static async getTotalColumnByDate(user_id, date, label) {
         try {
-            const query = `SELECT SUM(weight) AS total_weight FROM logged_workouts WHERE DATE(date) = '${date}';`;
+            const query = `SELECT SUM(${label}) AS total FROM logged_workouts WHERE DATE(date) = '${date}' AND user_id = '${user_id}';`;
+            const response = await db.one(query);
+            return response;
+        } catch(err) {
+            return err.message;
+        }
+    }
+
+    static async getTotalDurationByDate(date) {
+        try {
+            const query = `SELECT SUM(duration_min) AS total_duration_min FROM logged_workouts WHERE DATE(date) = '${date}';`;
+            const response = await db.one(query);
+            return response;
+        } catch(err) {
+            return err.message;
+        }
+    }
+    static async getTotalDistanceByDate(date) {
+        try {
+            const query = `SELECT SUM(distance) AS total_distance FROM logged_workouts WHERE DATE(date) = '${date}';`;
             const response = await db.one(query);
             return response;
         } catch(err) {
--- routes/charts.js
+++ routes/charts.js
@@ -25,27 +25,46 @@ router.get('/', (req, res, next) => {
 
 router.get('/:type_id', async (req, res, next) => {
     const { type_id } = req.params;
+    console.log(typeof type_id);
     const user_id = req.session.user_id;
+    const typeData = await workoutModel.getTypeInfo(type_id);
     var d = new Date();
-    let weights = [];
+    let sums = [];
     let date_labels = []
+    let label = null;
     for (let i = 0; i < 7; i++) {
         console.log(d);
-        d.to
-        const weight = await chartModel.getTotalWeightByDate(d.toString().substring(0, 16));
-        weights.unshift(Number(weight.total_weight));
         let d_curr = d.toDateString().substring(0, 11);
         date_labels.unshift(d_curr);
+        console.log("THIS IS MY TYPE ID:", type_id);
+        switch (type_id) {
+            case '1' :
+                label = "weight";
+                console.log("AM I IN CASE 1");
+                break;
+            case '2' :
+                label = "distance";
+                break;
+            case '4' :
+                label = "duration";
+                break;
+            default :
+            console.log("THIS IS THE CASE");
+                break;
+        }
+        console.log(label);
+        const sum = await chartModel.getTotalColumnByDate(user_id, d.toString().substring(0, 16), label);
+        sums.unshift(Number(sum.total));
         d.setDate(d.getDate() - 1);
     }
-    const typeData = await workoutModel.getTypeInfo(type_id);
-    console.log(weights);
+    console.log(sums);
     console.log(date_labels);
     res.render('template', {
         locals: {
             title: type_id,
             is_logged_in: req.session.is_logged_in,
-            weights,
+            sums,
+            label: JSON.stringify(label),
             date_labels: JSON.stringify(date_labels),
             typeData
         }, 
